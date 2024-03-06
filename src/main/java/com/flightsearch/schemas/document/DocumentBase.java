f08fef5c4886f9e0517d2ccbb74650bc8157084a
klim405 <klim405.develop@gmail.com>
2024-03-06 12:38:34 +0300

    feat: Добавлены схемы.

--- src/main/java/com/flightsearch/schemas/document/DocumentBase.java
+++ src/main/java/com/flightsearch/schemas/document/DocumentBase.java
@@ -3,8 +3,11 @@ package com.flightsearch.schemas.document;
 import com.flightsearch.models.Document;
 import com.flightsearch.schemas.ModelSchema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+@Data
 public class DocumentBase implements ModelSchema<Document> {
     @Length(max = 50)
     @NotBlank
@@ -13,9 +16,13 @@ public class DocumentBase implements ModelSchema<Document> {
     @NotBlank
     private String content;
 
+    @NotNull
+    private Long ownerId;
+
     @Override
     public Document toModel() {
         Document doc = new Document();
+        doc.setOwnerId(this.ownerId);
         doc.setTitle(this.title);
         doc.setContent(this.content);
         return doc;
@@ -25,11 +32,13 @@ public class DocumentBase implements ModelSchema<Document> {
     public void fromModel(Document document) {
         this.title = document.getTitle();
         this.content = document.getContent();
+        this.ownerId = document.getOwnerId();
     }
 
     @Override
     public void updateModel(Document document) {
         document.setTitle(this.title);
         document.setContent(this.content);
+        document.setOwnerId(this.ownerId);
     }
 }
