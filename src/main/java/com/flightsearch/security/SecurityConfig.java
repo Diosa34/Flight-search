4ce967112863ec362961d9d8232ef89fa0c26adc
klim405 <klim405.develop@gmail.com>
2024-04-16 22:26:22 +0300

    typo: Исправлены импорты и убраны ненужные комментарии.

--- src/main/java/com/flightsearch/security/SecurityConfig.java
+++ src/main/java/com/flightsearch/security/SecurityConfig.java
@@ -1,24 +1,16 @@
 package com.flightsearch.security;
 
-import com.flightsearch.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.AbstractJaasAuthenticationProvider;
-import org.springframework.security.authentication.jaas.AuthorityGranter;
-import org.springframework.security.authentication.jaas.DefaultJaasAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,11 +25,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
-                    .requestMatchers("/login").permitAll()
-                    .requestMatchers("/auth").permitAll()
-                    .requestMatchers("/user").permitAll()
-                    .requestMatchers("/swagger-ui/**").permitAll()
-                    .anyRequest().authenticated()
+                        .requestMatchers("/user").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
