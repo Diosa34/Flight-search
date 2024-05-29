c650c13277cb60cc9c21713103bb6098fcb46fe5
klim405 <klim405.develop@gmail.com>
2024-05-29 17:47:32 +0300

    refactor: Доработано логирование.

--- src/main/java/com/flightsearch/controllers/jms/JmsPayrollController.java
+++ src/main/java/com/flightsearch/controllers/jms/JmsPayrollController.java
@@ -3,10 +3,12 @@ package com.flightsearch.controllers.jms;
 import com.flightsearch.schemas.jms.PayrollCreate;
 import com.flightsearch.services.PayrollServerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile({"devPayroll", "prodPayroll"})
 @AllArgsConstructor
@@ -15,7 +17,8 @@ public class JmsPayrollController {
 
     @JmsListener(destination = "payroll")
     public void createPayroll(PayrollCreate schema) {
-        System.out.println("Получена запись payroll от " + schema.getAuthorId() + " для " + schema.getUserId());
+        log.info("Получен запрос на создание payroll от user({}) для user({})",
+                schema.getAuthorId(), schema.getUserId());
         payrollService.createPayroll(schema);
     }
 }
